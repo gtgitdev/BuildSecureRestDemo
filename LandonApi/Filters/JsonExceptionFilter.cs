874f9b9963b428aae343dc9d6d517f9af96b3727
gtgitdev <g.turton@hotmail.com>
2019-07-23 06:57:21 -0400

    added json exception filter

---
+++ LandonApi/Filters/JsonExceptionFilter.cs
@@ -0,0 +1,40 @@
+﻿using LandonApi.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace LandonApi.Filters
+{
+    public class JsonExceptionFilter : IExceptionFilter
+    {
+        private readonly IHostingEnvironment env;
+
+        public JsonExceptionFilter(IHostingEnvironment env)
+        {
+            this.env = env;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            var error = new ApiError();
+
+            if (env.IsDevelopment())
+            {
+                error.Message = context.Exception.Message;
+                error.Details = context.Exception.StackTrace;
+            }
+            else
+            {
+                error.Message = "A server error occured";
+                error.Details = context.Exception.Message;
+            }
+
+
+            context.Result = new ObjectResult(error)
+            {
+                StatusCode = 500
+            };
+
+        }
+    }
+}
--- LandonApi/Startup.cs
+++ LandonApi/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LandonApi.Filters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,7 +27,11 @@ namespace LandonApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options =>
+                {
+                    options.Filters.Add<JsonExceptionFilter>();
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddRouting(options => options.LowercaseUrls = true);
 
