dd9cf5d4bd2cc803eff593cefefe1c7b1657249e
gtgitdev <g.turton@hotmail.com>
2019-07-24 04:48:20 -0400

    Create resource base class

---
+++ LandonApi/Models/Resource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace LandonApi.Models
+{
+    public abstract class Resource
+    {
+        [JsonProperty(Order = -2)]
+        public string Href { get; set; }
+    }
+}
