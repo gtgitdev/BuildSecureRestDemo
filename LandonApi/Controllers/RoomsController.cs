7e5cdd2423066f77e284f250066f6967408a4168
gtgitdev <g.turton@hotmail.com>
2019-07-30 06:08:04 -0400

    add booking post method

--- LandonApi/Controllers/RoomsController.cs
+++ LandonApi/Controllers/RoomsController.cs
@@ -10,22 +10,28 @@ using Microsoft.Extensions.Options;
 
 namespace LandonApi.Controllers
 {
-
+    // TODO: Authentication
     [Route("/[controller]")]
     [ApiController]
     public class RoomsController : ControllerBase
     {
         private readonly IRoomService roomService;
         private readonly IOpeningService openingService;
+        private readonly IDateLogicService dateLogicService;
+        private readonly IBookingService bookingService;
         private readonly PagingOptions defaultPagingOptions;
 
         public RoomsController(
             IRoomService roomService,
             IOpeningService openingService,
-            IOptions<PagingOptions> defaultPagingOptions )
+            IOptions<PagingOptions> defaultPagingOptions,
+            IDateLogicService dateLogicService,
+            IBookingService bookingService)
         {
             this.roomService = roomService;
             this.openingService = openingService;
+            this.dateLogicService = dateLogicService;
+            this.bookingService = bookingService;
             this.defaultPagingOptions = defaultPagingOptions.Value;
         }
 
@@ -89,5 +95,36 @@ namespace LandonApi.Controllers
             return room;
         }
 
+        // Post /rooms/{roomId}/bookings
+        [HttpPost("{roomId}/bookings", Name = nameof(CreateBookingForRoom))]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(201)]
+        public async Task<ActionResult> CreateBookingForRoom(Guid roomId, [FromBody] BookingForm bookingForm)
+        {
+            var room = await roomService.GetRoomAsync(roomId);
+            if (room == null) return NotFound();
+
+            var minimumStay = dateLogicService.GetMinimumStay();
+            bool tooShort = (bookingForm.EndAt.Value - bookingForm.StartAt.Value) <
+                            minimumStay;
+            if (tooShort)
+                return BadRequest(new ApiError($"The minimum booking duration is {minimumStay.TotalHours} hours"));
+
+            var conflictedSlots =
+                await openingService.GetConflictingSlots(roomId, bookingForm.StartAt.Value, bookingForm.EndAt.Value);
+            if (conflictedSlots.Any()) return BadRequest(new ApiError("This time conflicts with existing booking."));
+
+            // TODO: Get the current user
+            var userId = Guid.NewGuid();
+
+            var bookingId = await bookingService.CreateBookingAsync(
+                userId, roomId, bookingForm.StartAt.Value, bookingForm.EndAt.Value);
+
+            return Created(
+                Url.Link(nameof(BookingsController.GetBookingById),
+                    new {bookingId}), null);
+
+        }
     }
 }
